41aabafb2aba9dffa4ca6f49821b43fdb41d08cc
kergb <daixuaner@gmail.com>
2025-07-20 00:50:41 +0800

    Delete Miss.js

--- Miss.js
+++
@@ -1,9 +0,0 @@
-// 正则：匹配目标站点
-const TARGET_SITES_REGEX = /(missav|netflav|supjav|njav|javday)/i;
-const JAVBUS_REGEX = /javbus/i;
-const HUARENLIVE_REGEX = /huaren\.live\/player\/ec\.php/i;
-
-// 正则：用于内容替换
-const TITLE_REGEX = /<\/title>/i;
-const BODY_REGEX = /<\/body>/i;
-const WINDOW_OPEN_REGEX = /window\.open/g;
