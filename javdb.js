a76d5769db524d584a63a2a0b462bba2b6900efc
kergb <daixuaner@gmail.com>
2025-07-20 14:01:50 +0800

    Create javdb.js

---
+++ javdb.js
@@ -0,0 +1,8 @@
+if (!/html>/.test($response.body)) $done({});
+// 去顶部域名,底部下载提醒,播放页广告
+const body = $response.body.replace(
+  /<\/head>/,
+  "<style> .sub-header, .app-desktop-banner, .moj-content {display:none!important;} </style> \n </head>"
+);
+
+$done({ body });
