2c44088470b593e19572ea2770fb6eeee3ea276c
kergb <daixuaner@gmail.com>
2026-08-10 03:52:38 +0800

    Create Ghhhhj.js

---
+++ Ghhhhj.js
@@ -0,0 +1,56 @@
+const html = $response.body;
+
+if (!html) {
+  $done({});
+}
+
+const parser = new DOMParser();
+const doc = parser.parseFromString(html, "text/html");
+
+
+// 删除广告 script
+doc.querySelectorAll("script").forEach(el=>{
+    let src = el.src || "";
+    let text = el.textContent || "";
+
+    if(
+        src.includes("tsyndicate.com") ||
+        text.includes("TSOutstreamVideo") ||
+        text.includes("htmlAds")
+    ){
+        el.remove();
+    }
+});
+
+
+// 删除广告节点
+const selectors = [
+    'a[href*="bit.ly/"]',
+    'div:has(a[href*="go.myavlive.com"])',
+    'div[style*="width: 300px; height: 250px;"]',
+    '.relative>div[x-init*="campaignId=under_player"]',
+    'div[x-show^="recommendItems"]~div[class]:has(a[rel^="sponsored"])'
+];
+
+
+selectors.forEach(sel=>{
+    try{
+        doc.querySelectorAll(sel).forEach(e=>e.remove());
+    }catch(e){}
+});
+
+
+// 防弹窗
+const script = doc.createElement("script");
+script.textContent = `
+(function(){
+window.open=function(){};
+})();
+`;
+
+doc.head.appendChild(script);
+
+
+$done({
+    body:"<!DOCTYPE html>"+doc.documentElement.outerHTML
+});
