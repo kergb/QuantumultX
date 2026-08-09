d1dd6dc476593fb984faa7798a9734f075c5b66f
kergb <daixuaner@gmail.com>
2026-08-10 05:30:19 +0800

    Update Hi.js

--- Hi.js
+++ Hi.js
@@ -1,94 +1,56 @@
-/**
- * Missav 广告清理
- * ECMAScript Rewrite Script
- */
+const html = $response.body;
 
-let body = $response.body;
-
-if (!body) {
+if (!html) {
   $done({});
 }
 
+const parser = new DOMParser();
+const doc = parser.parseFromString(html, "text/html");
+
+
 // 删除广告 script
-body = body.replace(
-  /<script[^>]*?(?:tsyndicate\.com|TSOutstreamVideo|htmlAds)[\s\S]*?<\/script>/gi,
-  ""
-);
+doc.querySelectorAll("script").forEach(el=>{
+    let src = el.src || "";
+    let text = el.textContent || "";
 
-
-// 删除 go.myavlive.com 广告块
-body = body.replace(
-  /<div[^>]*>[\s\S]*?go\.myavlive\.com[\s\S]*?<\/div>/gi,
-  ""
-);
-
-
-// 删除 bit.ly 推广
-body = body.replace(
-  /<a[^>]*href=["'][^"']*bit\.ly[^"']*["'][^>]*>[\s\S]*?<\/a>/gi,
-  ""
-);
-
-
-// 删除 300x250 广告
-body = body.replace(
-  /<div[^>]*width:\s*300px;\s*height:\s*250px;[^>]*>[\s\S]*?<\/div>/gi,
-  ""
-);
-
-
-// 注入 CSS
-const css = `
-.lg\\:block,
-.lg\\:hidden,
-a[href*="//bit.ly/"],
-div[x-init*="#genki-counter"],
-div:has(a[href*="go.myavlive.com"]),
-[x-show$="video_details']>div>ul"],
-div[style*="width: 300px; height: 250px;"],
-.relative>div[x-init*="campaignId=under_player"],
-div[x-show^="recommendItems"]~div[class]:has(
->div>div.mx-auto>div.flex>a[rel^="sponsored"]
-){
-display:none!important;
-}
-`;
-
-body = body.replace(
-"</head>",
-`<style>${css}</style></head>`
-);
-
-
-// 注入防弹窗 JS
-const js = `
-<script>
-(function(){
-document.addEventListener("DOMContentLoaded",function(){
-
-window.open=function(){};
-
-if(window.player && window.player.pause){
-let oldPause=window.player.pause;
-
-window.player.pause=function(){
-if(document.hasFocus()){
-oldPause();
-}
-}
-}
-
+    if(
+        src.includes("tsyndicate.com") ||
+        text.includes("TSOutstreamVideo") ||
+        text.includes("htmlAds")
+    ){
+        el.remove();
+    }
 });
+
+
+// 删除广告节点
+const selectors = [
+    'a[href*="bit.ly/"]',
+    'div:has(a[href*="go.myavlive.com"])',
+    'div[style*="width: 300px; height: 250px;"]',
+    '.relative>div[x-init*="campaignId=under_player"]',
+    'div[x-show^="recommendItems"]~div[class]:has(a[rel^="sponsored"])'
+];
+
+
+selectors.forEach(sel=>{
+    try{
+        doc.querySelectorAll(sel).forEach(e=>e.remove());
+    }catch(e){}
+});
+
+
+// 防弹窗
+const script = doc.createElement("script");
+script.textContent = `
+(function(){
+window.open=function(){};
 })();
-</script>
 `;
 
-body = body.replace(
-"</body>",
-js+"</body>"
-);
+doc.head.appendChild(script);
 
 
 $done({
-body
+    body:"<!DOCTYPE html>"+doc.documentElement.outerHTML
 });
