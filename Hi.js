2f1c983175d16bbfc123562a83b89404ec4e28d6
kergb <daixuaner@gmail.com>
2026-08-10 05:25:33 +0800

    Update Hi.js

--- Hi.js
+++ Hi.js
@@ -27,48 +27,94 @@ doc.querySelectorAll("script").forEach(el => {
 });
 
 
-// 删除确定广告节点
+// 删除明确广告节点
 
-const selectors = [
+[
+'a[href*="//bit.ly/"]',
+'div:has(a[href*="go.myavlive.com"])',
+'div[style*="width: 300px; height: 250px;"]',
+'.relative>div[x-init*="campaignId=under_player"]',
+'div[x-show^="recommendItems"]~div[class]:has(a[rel^="sponsored"])'
+]
+.forEach(sel=>{
 
-    'a[href*="//bit.ly/"]',
-
-    'div:has(a[href*="go.myavlive.com"])',
-
-    'div[style*="width: 300px; height: 250px;"]',
-
-    '.relative>div[x-init*="campaignId=under_player"]',
-
-    'div[x-show^="recommendItems"]~div[class]:has(a[rel^="sponsored"])'
-
-];
-
-
-selectors.forEach(sel => {
-
-    try {
-
+    try{
         doc.querySelectorAll(sel)
-        .forEach(el => el.remove());
-
-    } catch(e){}
+        .forEach(e=>e.remove());
+    }catch(e){}
 
 });
 
 
-// 禁止弹窗
+// 注入 CSS 隐藏残留
+
+const style = doc.createElement("style");
+
+style.textContent = `
+
+.lg\\:block,
+.lg\\:hidden,
+a[href*="//bit.ly/"],
+div[x-init*="#genki-counter"],
+div:has(a[href*="go.myavlive.com"]),
+[x-show$="video_details"]>div>ul,
+div[style*="width: 300px; height: 250px;"],
+.relative>div[x-init*="campaignId=under_player"],
+div[x-show^="recommendItems"]~div[class]:has(>div>div.mx-auto>div.flex>a[rel^="sponsored"])
+{
+display:none!important;
+}
+
+`;
+
+doc.head.appendChild(style);
+
+
+// 注入动态清理 JS
 
 const script = doc.createElement("script");
 
 script.textContent = `
+
 (function(){
+
+function clean(){
+
+[
+'a[href*="//bit.ly/"]',
+'a[href*="go.myavlive.com"]',
+'.under_player'
+].forEach(s=>{
+
+document.querySelectorAll(s)
+.forEach(e=>e.remove());
+
+});
+
+}
+
+clean();
+
+new MutationObserver(clean)
+.observe(
+document.documentElement,
+{
+childList:true,
+subtree:true
+}
+);
+
+
 window.open=function(){};
+
+
 })();
+
 `;
 
 doc.head.appendChild(script);
 
 
 $done({
-    body:"<!DOCTYPE html>"+doc.documentElement.outerHTML
+body:"<!DOCTYPE html>"+doc.documentElement.outerHTML
 });
